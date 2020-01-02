4b14342cb33ea7e0f46e9b79ef2bb6f9b8f717d9
Julian <jumar98@hotmail.com>
2020-01-01 22:13:41 -0500

    [feat] Adding new view to list posts

--- JDGram/posts/views.py
+++ JDGram/posts/views.py
@@ -1,3 +1,38 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from datetime import datetime
+
+
+posts = [
+    {
+        'title': "Forget everything that you think you know",
+        'user': {
+            'name': "Dr. Strange",
+            'picture': 'https://codigoespagueti.com/wp-content/uploads/2017/08/thor-strange-1080x600.png'
+        },
+        'timestamp': datetime.now().strftime('%b %dTh, %Y - %H:%M Hrs'),
+        'photo': 'https://codigoespagueti.com/wp-content/uploads/2017/08/thor-strange-1080x600.png'
+    },
+    {
+        'title': "I am Iron Man",
+        'user': {
+            'name': "Iron Man",
+            'picture': 'https://cdn.colombia.com/sdi/2018/09/04/avengers-4-quien-sera-el-sucesor-de-iron-man-668712.jpg'
+        },
+        'timestamp': datetime.now().strftime('%b %dTh, %Y - %H:%M Hrs'),
+        'photo':'https://cdn.colombia.com/sdi/2018/09/04/avengers-4-quien-sera-el-sucesor-de-iron-man-668712.jpg'    
+    },
+    {
+        'title': "You can't justify murder by masking it with a cause",
+        'user':{
+            'name': "Captain America",
+            'picture': 'https://www.screengeek.net/wp-content/uploads/2019/11/avengers-endgame-captain-america.jpg'
+        },
+        'timestamp': datetime.now().strftime('%b %dTh, %Y - %H:%M Hrs'),
+        'photo': 'https://www.screengeek.net/wp-content/uploads/2019/11/avengers-endgame-captain-america.jpg'
+    }
+]
 
 # Create your views here.
+def list_posts(requests):
+    return render(requests, 'feed.html', {'posts': posts})
