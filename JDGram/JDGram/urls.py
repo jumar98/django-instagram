fa4617e445d2dd48630a5796f27ce360bda4007a
Julian <jumar98@hotmail.com>
2020-01-05 08:36:59 -0500

    [feat] We add logout for users

--- JDGram/JDGram/urls.py
+++ JDGram/JDGram/urls.py
@@ -17,11 +17,12 @@ from django.contrib import admin
 from django.conf import settings
 from django.urls import path
 from posts import views
-from users.views import login_view 
+from users.views import login_view, logout_view
 from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('posts/', views.list_posts, name='posts'),
-    path('users/login/', login_view, name='login' )
+    path('users/login/', login_view, name='login'),
+    path('users/logout/', logout_view, name='logout')
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
