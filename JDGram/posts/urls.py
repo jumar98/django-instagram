ca951f4e4b2228630788f4c4c71e7c85e8876ae9
Julian <jumar98@hotmail.com>
2020-01-06 20:04:19 -0500

    [feat] Adding pagination and refactoring code (DRY)

--- JDGram/posts/urls.py
+++ JDGram/posts/urls.py
@@ -1,8 +1,9 @@
 
 from django.urls import path
-from posts.views import PostListView, create_post
+from posts.views import PostListView, create_post, PostDetailView
 
 urlpatterns = [
     path('', PostListView.as_view(), name='posts'),
-    path('new/', create_post, name='create_post'),
+    path('posts/new/', create_post, name='create_post'),
+    path('posts/<int:pk>/', PostDetailView.as_view(), name='detail')
 ]
--- JDGram/posts/views.py
+++ JDGram/posts/views.py
@@ -4,7 +4,7 @@ from datetime import datetime
 from django.contrib.auth.decorators import login_required
 from posts.forms import PostForm
 from posts.models import Post
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 from django.contrib.auth.mixins import LoginRequiredMixin
 # Create your views here.
 
@@ -13,9 +13,15 @@ class PostListView(LoginRequiredMixin, ListView):
     template_name = 'posts/feed.html'
     model = Post
     ordering = ('-created',)
-    paginate_by = 2
+    paginate_by = 1
     context_object_name = 'posts'
 
+class PostDetailView(LoginRequiredMixin, DetailView):
+
+    template_name = 'posts/detail.html'
+    queryset = Post.objects.all()
+    context_object_name = 'post'
+    
 @login_required
 def create_post(request):
     if request.method == 'POST':
